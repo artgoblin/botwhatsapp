02f920d0d03f96ee74975d20579e29ba86787e61
artgoblin <72606593+artgoblin@users.noreply.github.com>
2021-02-28 13:41:22 +0530

    added dialogflow

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
+from utils import fetch_reply 
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,13 +14,15 @@ def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
     msg = request.form.get('Body')
+    phone_no=request.form.get('From')
+    reply= fetch_reply(msg,phone_no)
 
     # Create reply
     resp = MessagingResponse()
-    resp.message("hahahahah : {}".format(msg))
+    resp.message(reply)
 
-    return str(resp)
+    return str("artgoblin:",resp)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run()
 
