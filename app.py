8d0fac950477a20f9315b462f7b797e1b2231d41
artgoblin <72606593+artgoblin@users.noreply.github.com>
2021-02-28 22:31:37 +0530

    update

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def helpaa():
     "3>To relax-(Funn-time)\n"\
     "4>To get the routine-(Show-routine)\n"\
     "5>To get random jokes- just mention the word jokes in your sentance or say (yes)\n"\
-    "6>To get the link of syllabus-(//syl)"
+    "6>To get the link of syllabus-(//syl)\n"\
     "have a great day thanks for using and also there are many secrets ....have fun discovering them ;-)....."
     return hel
 
