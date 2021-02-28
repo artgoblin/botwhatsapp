cd031661a61e1508bf2ae4390a3b47408600da6d
artgoblin <72606593+artgoblin@users.noreply.github.com>
2021-02-28 21:13:47 +0530

    updates

--- app.py
+++ app.py
@@ -42,26 +42,26 @@ def sms_reply():
     return str(resp)
 
 def meetinglinks():
-    meetings="meeting links\n\n" \
+    meet="meeting links\n\n" \
     "1>*analogue ECE-494 lab(abhijit and bholanath sir)*=https://us04web.zoom.us/j/6182151842?pwd=VFczNVVWKzVHQmdEYXh0VUltL1k4UT09\n"\
     "2>*analogue ECE-404 theory(abhijit sir)*=https://us04web.zoom.us/j/6182151842?pwd=VFczNVVWKzVHQmdEYXh0VUltL1k4UT09\n"\
     "3>*digital ECE-403 theory(moupali ma'am)*=https://zoom.us/j/9939445614?pwd=ZFZ4ZkprU3V6eXdSQU5VMGtwNytjdz09\n"\
     "4>*digital ECE-493 lab(moupali ma'am and atunu sir)*=https://docs.google.com/forms/d/e/1FAIpQLSeSFjZTw71l4-kKSbcWsXa7SPa08y0LqskxttrCFCHzG7cirA/viewform?usp=sf_link\n"
     "5>*signals and systems ECE-401 (pronob sir)*=https://us02web.zoom.us/j/9695083521 \n"
-    return meetings
+    return meet
 
 
 def attendancelinks():
-    attendance="attendance links\n\n\n"\
+    atten="attendance links\n\n\n"\
     "1>*electronics devices and ckt(surojit sir)*=https://forms.gle/rad9YeTFwJPEyoEu7\n\n"\
     "2>*digital ECE-403 theory(moupali ma'am)*=https://docs.google.com/forms/d/e/1FAIpQLSep79lE8_oG2HgfI0TVEnCwEEl96drlQoF7D8BpvSPJthubBw/viewform?usp=sf_link\n\n"\
     "3>*digital ECE-493 lab(moupali ma'am and atunu sir)*=https://docs.google.com/forms/d/e/1FAIpQLSeSFjZTw71l4-kKSbcWsXa7SPa08y0LqskxttrCFCHzG7cirA/viewform?usp=sf_link\n\n"
-    return attendance 
+    return atten
            
 
 
 def artgoblin():
-    artgoblin="this is artgoblin's "
+    artgoblin="this is artgoblin's work"
     return artgoblin
 
 if __name__ == "__main__":
