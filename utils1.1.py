253697825e9c314bf51baec617c6c10b9a614de1
artgoblin <72606593+artgoblin@users.noreply.github.com>
2021-03-02 11:12:37 +0530

    some more updates

---
+++ utils1.1.py
@@ -0,0 +1,18 @@
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "quick-elixir-306207-fd4c678ea7cc.json"
+
+import dialogflow_v2 as dialogflow
+dialogflow_session_client = dialogflow.SessionsClient()
+PROJECT_ID = "simpybo-phqj"
+
+def detect_intent_from_text(text, session_id, language_code='en'):
+    session = dialogflow_session_client.session_path(PROJECT_ID, session_id)
+    text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.types.QueryInput(text=text_input)
+    response = dialogflow_session_client.detect_intent(session=session, query_input=query_input)
+    return response.query_result
+
+def reply_music(query,session_id):
+    response=detect_intent_from_text(query,session_id)
+    return response.fulfillment_text
