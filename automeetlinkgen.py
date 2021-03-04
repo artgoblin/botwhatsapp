5d793973f96f6abbc6d59d4cd863ab339aeb83d1
artgoblin <72606593+artgoblin@users.noreply.github.com>
2021-03-04 12:29:45 +0530

    experiment

--- automeetlinkgen.py
+++ automeetlinkgen.py
@@ -5,13 +5,15 @@ from selenium import webdriver
    
 op = webdriver.Chromedriver
 op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-op.add_argumnet("--disable-gpu")
-op.add_argument('--no-sandbox')
+op.add_argumnet("window-size=1920x1480")
+op.add_argument("disable-dev-shm-usage")
+driver = webdriver.Chrome(executable_path= os.environ.get('CHROMEDRIVER_PATH'), chrome_options=op)
+driver.implicitly_wait(20)
 
 def replya():
     
-    driver = webdriver.Chrome(executable_path= os.environ.get('CHROMEDRIVER_PATH'), chrome_options=op)
-    driver.implicitly_wait(20)
+    
+    
     driver.get("https://meet.google.com/")
 
     #clicking the meeting tab
@@ -21,7 +23,7 @@ def replya():
 
     #to sign in
     password = "s1234DAS2000"
-"
+
     email= "projectsmail768@gmail.com"
     driver.find_element_by_xpath('//*[@id="identifierId"]').send_keys(email)
     driver.find_element_by_id('identifierNext').click()
